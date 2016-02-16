b6c256024bd5a515feda0aec5eaa0aea6259bb2d
Shanmuga <shanmuga.chidambaravel@gmail.com>
2016-02-16 19:50:31 +0530

    authentication added

--- simple_pyramid_app/__init__.py
+++ simple_pyramid_app/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+from pyramid.authentication import AuthTktAuthenticationPolicy
 
 from .models import (
     DBSession,
@@ -13,7 +14,8 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    config = Configurator(settings=settings)
+    authnpolicy = AuthTktAuthenticationPolicy('seekrit', hashalg='sha512')
+    config = Configurator(settings=settings, authentication_policy=authnpolicy)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
@@ -21,6 +23,8 @@ def main(global_config, **settings):
     config.add_route('student_list', '/students')
     config.add_route('add_student', '/student/add', request_method=['GET', 'POST'])
     config.add_route('student_detail', '/student/{id}')
+    config.add_route('login','/login', request_method=['GET', 'POST'])
+    config.add_route('logout', '/logout')
 
     config.scan()
     return config.make_wsgi_app()
--- simple_pyramid_app/views.py
+++ simple_pyramid_app/views.py
@@ -1,3 +1,4 @@
+from pyramid.security import remember, forget
 from pyramid.httpexceptions import HTTPFound
 import transaction
 from pyramid.renderers import render
@@ -12,28 +13,28 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home')
 def home_view(request):
-    html = render("templates/home.jinja2", {})
+    html = render("templates/home.jinja2", {'authenticated_student_id': request.authenticated_userid})
     return Response(html)
 
 @view_config(route_name="student_list")
 def students_view(request):
     students = DBSession.query(Student).all()
-    html = render("templates/student_list.jinja2", {'all_students':students})
+    html = render("templates/student_list.jinja2", {'all_students':students, 'authenticated_student_id': request.authenticated_userid})
     return Response(html)
 
 @view_config(route_name='student_detail')
 def student_detail_view(request):
     id = request.matchdict['id']
     student = DBSession.query(Student).filter(Student.id==int(id)).one_or_none()
-    html = render("templates/student_details.jinja2", {'student': student})
+    html = render("templates/student_details.jinja2", {'student': student, 'authenticated_student_id': request.authenticated_userid})
     status = 200 if student is not None else 400
     return Response(html, status=status)
 
 @view_config(route_name="add_student", request_method="GET")
 def add_student_view(request):
-    html = render("templates/add_student.jinja2", {})
+    html = render("templates/add_student.jinja2", {'authenticated_student_id': request.authenticated_userid})
     return Response(html)
 
 
@@ -41,8 +42,36 @@ def add_student_view(request):
 def persist_student(request):
     student_name = request.POST['student_name']
     student_gender = request.POST['student_gender']
-    student = Student(name=student_name, gender=student_gender)
+    pwd = request.POST['pwd']
+    student = Student(name=student_name, gender=student_gender, password=pwd)
     with transaction.manager:
         DBSession.add(student)
         transaction.commit()
     return HTTPFound(request.route_url('home'))
+
+@view_config(route_name='login', request_method='GET')
+def login_view(request):
+    html = render('templates/login.jinja2', {'authenticated_student_id': request.authenticated_userid})
+    return Response(html)
+
+@view_config(route_name='login', request_method='POST')
+def authenticate(request):
+    student_name = request.POST['student_name']
+    pwd = request.POST['pwd']
+    student = DBSession.query(Student).filter(Student.name==student_name and Student.password==pwd).one_or_none()
+    if student is not None:
+        id_str = str(student.id)
+        headers = remember(request, id_str)
+        html = render("templates/home.jinja2", {'authenticated_student_id': id_str})
+        return Response(body=html, headers=headers)
+
+    else:
+        html = render('templates/login.jinja2', {'authenticated_student_id': request.authenticated_userid})
+        return Response(html)
+
+
+@view_config(route_name='logout')
+def logout(request):
+    header = forget(request)
+    html = render('templates/login.jinja2', {'authenticated_student_id': None})
+    return Response(html, headers=header)
